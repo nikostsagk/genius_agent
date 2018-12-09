c41d3efa012c080b61ba490e08f3fc8120db2750
dda410 <dimitri1215@gmail.com>
2018-12-09 19:13:26 +0100

    Acceptance now based only on social wealfare

--- src/ExampleAgent_with_uncertainty_experimental.java
+++ src/ExampleAgent_with_uncertainty_experimental.java
@@ -165,8 +165,8 @@ public class ExampleAgent extends AbstractNegotiationParty {
 	    // if the utility of the bid is higher than Example Agent's last bid.
 	    if (lastReceivedOffer != null
 		                && myLastOffer != null
-		&& this.utilitySpace.getUtility(lastReceivedOffer) * opponentsAdditiveUtilitySpace.getUtility(lastReceivedOffer) > this.utilitySpace.getUtility(myLastOffer) * opponentsAdditiveUtilitySpace.getUtility(myLastOffer)
-		&& this.getUtility(lastReceivedOffer) >= this.getUtility(myLastOffer)){
+		&& this.utilitySpace.getUtility(lastReceivedOffer) * opponentsAdditiveUtilitySpace.getUtility(lastReceivedOffer) > this.utilitySpace.getUtility(myLastOffer) * opponentsAdditiveUtilitySpace.getUtility(myLastOffer) ){
+		//                && this.getUtility(lastReceivedOffer) >= this.getUtility(myLastOffer)) {
 		//                myLastOffer = generateBestBidWithinRangeForOpponent((double) 1 - Math.pow(time, 1 / psi));
 		//            return new Offer(this.getPartyId(), myLastOffer);
 		return new Accept(this.getPartyId(), lastReceivedOffer);
